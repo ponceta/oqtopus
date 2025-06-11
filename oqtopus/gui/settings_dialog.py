51a58506f04af56300ec29d044f3cf8dd0a3479a
Damiano Lombardi <damiano@opengis.ch>
2025-06-11 15:46:00 +0200

    Tooltip documentation for access token

--- oqtopus/gui/settings_dialog.py
+++ oqtopus/gui/settings_dialog.py
@@ -11,6 +11,16 @@ class SettingsDialog(QDialog, DIALOG_UI):
         self.setupUi(self)
 
         self.githubToken_lineEdit.setText(PluginUtils.get_github_token())
+        self.githubToken_lineEdit.setToolTip(
+            "<b>GitHub Access Token</b><br>"
+            "A personal access token is required to access private repositories or to increase API rate limits.<br><br>"
+            "To generate a token:<br>"
+            "1. Go to <a href='https://github.com/settings/tokens'>GitHub Personal Access Tokens</a>.<br>"
+            "2. Click <b>Generate new token</b>.<br>"
+            "3. Select the <code>repo</code> scope for most operations.<br>"
+            "4. Copy and paste the generated token here."
+        )
+        self.githubToken_label.setToolTip(self.githubToken_lineEdit.toolTip())
 
     def accept(self):
         PluginUtils.set_github_token(self.githubToken_lineEdit.text())
