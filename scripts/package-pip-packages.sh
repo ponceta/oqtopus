c5949681be6842658bc1825973a8c0b55a07ae74
Denis Rouzaud <denis.rouzaud@gmail.com>
2025-06-14 22:27:35 +0200

    package

--- scripts/package-pip-packages.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-pip download -r requirements.txt --only-binary :all: -d temp/
-            unzip -o "temp/*.whl" -d oqtopus/libs
-            rm -r temp
-            # set write rights to group (because qgis-plugin-ci needs it)
-            chmod -R g+w oqtopus/libs
